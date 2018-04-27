ea85c391377e3c087f3daf8d46e2c886f87def8d
lio30 <38773578+lio30@users.noreply.github.com>
2018-04-27 14:27:08 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <ul>
     <li>Carlos Henostroza Ramos - 1112200193</li>
-    <li>Gustavo Lavado Quispe - 0200213180 - GUSTAVO</li>
-    <li>elio morales cordova - 2006275013</li>
+    <li>Gustavo Lavado Quispe - 0200213180</li>
+    <li>elio morales cordova- EMC - 2006275013</li>
 </ul>
